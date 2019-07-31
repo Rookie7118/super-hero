9f315713aa3464a386fc578be48f1760e110c09d
lihongjie <don@aviagames.com>
2019-07-31 21:25:59 +0800

    initial commit

---
+++ common/common.js
@@ -0,0 +1,8 @@
+const serverUrl = "https://www.imovietrailer.com/superhero";
+// const serverUrl = "https://www.imovietrailer-dev.com/superhero";
+
+
+export default {
+	serverUrl,
+	serverUrlDev
+}
---
+++ pages/index/index.vue
@@ -0,0 +1,62 @@
+<template>
+	<view class="page">
+		<!-- 轮播图 start -->
+		<swiper :indicator-dots="true" :autoplay="false" circular="true" class="carousel">
+			<swiper-item v-for="carousel in carouselList">
+				<image :src="carousel.image" class="carousel"></image>
+			</swiper-item>
+		</swiper>
+		<!-- 轮播图 end -->
+		
+		<!-- 热门超英 start -->
+		<view class="page-block super-hot">
+			<view class="">
+				<image src="../../static/icos/hot.png" mode=""></image>
+				<view class="">
+					超英热门
+				</view>
+			</view>
+		</view>
+		
+		<!-- 热门超英 end -->
+		
+	</view>
+</template>
+
+<script>
+	
+	import localCarouselList from "../../static/json/carousel_list.json"
+	// import common from "../../common/common.js"
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				carouselList:[]
+			}
+		},
+		onLoad() {
+			// var serverUrl = common.serverUrl;
+			uni.request({
+				url: this.serverUrl + '/index/carousel/list',
+				method: 'POST',
+				success: res => {
+					if (res.statusCode == 200) {
+						res.data = localCarouselList;
+						this.carouselList = res.data.data;
+					} 
+					
+				},
+				fail: () => {},
+				complete: () => {}
+			});
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	@import url("index.css");
+</style>
